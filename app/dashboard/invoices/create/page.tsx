7ed47802e506efef1aaa48fcf71690c27be2f44b
masudursourav <it19025@mbstu.ac.bd>
2024-01-08 13:56:46 +0600

    8/1/24 - 2

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,4 +1,5 @@
 import { fetchCustomers } from '@/app/lib/data';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/create-form';
 
 export default async function Page() {
@@ -6,6 +7,16 @@ export default async function Page() {
 
   return (
     <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
       <Form customers={customers} />
     </main>
   );
