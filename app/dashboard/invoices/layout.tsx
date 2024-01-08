6592064c7057cde573134c2085ffe03a9036e16e
masudursourav <it19025@mbstu.ac.bd>
2024-01-08 14:22:57 +0600

    8/1/24 - 3

--- app/dashboard/invoices/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="grow border-4 border-rose-500 p-6 md:overflow-y-auto md:p-12	">
-      {children} Hello world from invoice layout
-    </div>
-  );
-}
